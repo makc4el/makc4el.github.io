a15b1a578b74df84c5421a288e83fdbb36302ca2
Makc Odarchenko <Makc Odarchenko>
2017-01-05 02:17:49 +0200

    fix

--- slice/gulp/js/script.js
+++ slice/gulp/js/script.js
@@ -16,4 +16,7 @@ $(document).ready(function(){
  		nativeOnMobile: false,
  		arrowButtonMarkup: '<span class="left-bar-btn" ><img clas="header-serach-icon" src="images/icons/arr.svg"/></span>' 		
  	});
+ 	$( "#datepicker" ).datepicker();
+ 	$( "#spinner1" ).spinner();
+ 	$( "#spinner2" ).spinner();
 })
